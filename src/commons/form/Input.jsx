1d66970e963f47be498e553d97956c2733b0345e
Xelem <anselmchizurum@gmail.com>
2022-10-07 13:12:28 +0100

    Created a reusable label and input component

--- src/commons/form/Input.jsx
+++ src/commons/form/Input.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Label from "./label";
 
-const Input = () => {
-    return <>{/* Input */}</>;
+const Input = (
+  labelFor,
+  type,
+  style,
+  label,
+  placeholder,
+  value,
+  loading,
+  disabled,
+  required,
+  name,
+  id
+) => {
+  return (
+    <>
+      <Label htmlFor={labelFor} required={required} />
+
+      <input
+        type={type}
+        aria-label={label}
+        placeholder={placeholder}
+        style={style}
+        value={value}
+        loading={loading}
+        disabled={disabled}
+        required={required}
+        name={name}
+        id={id}
+      />
+    </>
+  );
 };
 
 export default Input;
