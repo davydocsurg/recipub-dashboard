5ef3c9f548eb7aa09966402d6bcea8290d8c2cac
Japheth-Joepari <phython023@gmail.com>
2022-10-07 13:17:39 +0100

    added a resuable  button

--- src/commons/form/Button.jsx
+++ src/commons/form/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Button = () => {
-    return <>{/* button */}</>;
+const Button = ({ type, style, text, loading, disabled }) => {
+  return (
+    <React.Fragment>
+      <button
+        type={type}
+        style={style}
+        text={text}
+        loading={loading}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default Button;
