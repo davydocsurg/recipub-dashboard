6f41df425f1918baed8ec8c859bb56e623a2011b
davydocsurg <davydocsurg@gmail.com>
2022-10-08 17:33:04 +0100

    add textarea input changes

--- src/commons/form/TextArea.jsx
+++ src/commons/form/TextArea.jsx
@@ -1,31 +1,46 @@
 import React from "react";
+import FieldError from "./FieldError";
+import Label from "./label";
 
 const TextArea = ({
-  label,
-  type,
-  placeholder,
-  style,
-  value,
-  disabled,
-  required,
-  name,
-  id,
+    labelFor,
+    type,
+    placeholder,
+    style,
+    value,
+    disabled,
+    required,
+    name,
+    id,
+    error,
 }) => {
-  return (
-    <>
-      <textarea
-        className={style}
-        label={label}
-        type={type}
-        placeholder={placeholder}
-        disabled={disabled}
-        required={required}
-        name={name}
-        id={id}
-      >
-        {value}
-      </textarea>
-    </>
-  );
+    return (
+        <>
+            <div className="form-group">
+                {labelFor && (
+                    <Label
+                        htmlFor={name}
+                        inputName={labelFor}
+                        required={required}
+                    />
+                )}
+                <div className="mt-1">
+                    <textarea
+                        className={"form-control " + style}
+                        labelFor={labelFor}
+                        type={type}
+                        placeholder={placeholder}
+                        disabled={disabled}
+                        required={required}
+                        name={name}
+                        id={id}
+                    >
+                        {value}
+                    </textarea>
+                </div>
+            </div>
+            {error && <FieldError>{error}</FieldError>}
+        </>
+    );
 };
 export default TextArea;
--- src/views/UI.jsx
+++ src/views/UI.jsx
@@ -5,74 +5,77 @@ import SelectInput from "../commons/form/Select";
 import TextArea from "../commons/form/TextArea";
 import Login from "../components/superAdmin/auth/Login";
 
-
 const UI = () => {
-  const required = true;
-  const [options, setOptions] = useState([
-    { option: "male", value: "male" },
-    { option: "female", value: "female" },
-    { option: "other", value: "other" },
-  ]);
+    const required = true;
+    const [options, setOptions] = useState([
+        { option: "male", value: "male" },
+        { option: "female", value: "female" },
+        { option: "other", value: "other" },
+    ]);
 
-  return (
-    <>
-      <div className="container">
-        <h2 className="text-center">Components</h2>
-        <div className="row mt-5">
-          <div className="col-lg-4 col-md-6 col-sm-12">
-            <div className="card card-body">
-              <h5 className="">Input</h5>
-              <Input
-                id="name"
-                labelFor="Name"
-                type="text"
-                placeholder="Name"
-                name="name"
-                required={required}
-                error={"This field is required"}
-              />
-            </div>
-          </div>
+    return (
+        <>
+            <div className="container">
+                <h2 className="text-center">Components</h2>
+                <div className="row mt-5">
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="card card-body">
+                            <h5 className="">Input</h5>
+                            <Input
+                                id="name"
+                                labelFor="Name"
+                                type="text"
+                                placeholder="Name"
+                                name="name"
+                                required={required}
+                                error={"This field is required"}
+                            />
+                        </div>
+                    </div>
 
-          <div className="col-lg-4 col-md-6 col-sm-12">
-            <div className="card card-body">
-              <h5 className="">Button</h5>
-              <Button text="Login" style={"btn-primary btn-lg"} />
-            </div>
-          </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="card card-body">
+                            <h5 className="">Button</h5>
+                            <Button text="Login" style={"btn-primary btn-lg"} />
+                        </div>
+                    </div>
 
-          <div className="col-lg-4 col-md-6 col-sm-12">
-            <div className="card card-body">
-              <h5 className="">Select Input</h5>
-              <SelectInput
-                id="Gender"
-                labelFor="Gender"
-                name="gender"
-                options={options}
-                setOptions={setOptions}
-                required={required}
-                error={"Gender is required"}
-              />
-            </div>
-          </div>
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="card card-body">
+                            <h5 className="">Select Input</h5>
+                            <SelectInput
+                                id="Gender"
+                                labelFor="Gender"
+                                name="gender"
+                                options={options}
+                                setOptions={setOptions}
+                                required={required}
+                                error={"Gender is required"}
+                            />
+                        </div>
+                    </div>
 
-          <div className="col-lg-4 col-md-6 col-sm-12">
-            <div className="card card-body">
-              <TextArea
-                labelFor=""
-                type=""
-                placeholder="Default Parameter"
-                style=""
-                value=""
-                name=""
-                id=""
-                disabled={false}
-                required={true}
-              />
+                    <div className="col-lg-4 col-md-6 col-sm-12">
+                        <div className="card card-body">
+                            <h5 className="">TextArea</h5>
+                            <TextArea
+                                labelFor="Content"
+                                type=""
+                                placeholder="Default Parameter"
+                                style=""
+                                error={
+                                    "Content must not be greater than 250 characters"
+                                }
+                                name="content"
+                                id=""
+                                disabled={false}
+                                required={true}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-    </>
-  );
+        </>
+    );
 };
-
 export default UI;
