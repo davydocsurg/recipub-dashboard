4373af068a2957bc59c62fc18007571b553d82d4
Xelem <anselmchizurum@gmail.com>
2022-10-08 16:50:20 +0100

    Centered the login form in the admin page

--- src/views/superAdmin/Login.jsx
+++ src/views/superAdmin/Login.jsx
@@ -3,10 +3,10 @@ import Login from "../../components/superAdmin/auth/Login";
 const SuperAdminLogin = () => {
   return (
     <div className="container mt-4">
-      <div className="row justify-content-center">
-        <div className="col-md-6 border px-5 pt-5">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-md-6 border px-5 pt-5 pb-2">
           <h3 className="text-primary text-center">Super Admin Login</h3>
-          <Login />;
+          <Login />
         </div>
       </div>
     </div>
