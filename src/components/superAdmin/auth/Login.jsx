e6d92a844bfbab9e60878f06b2a59bec1c74a3c9
davydocsurg <davydocsurg@gmail.com>
2022-10-08 17:10:49 +0100

    add super admin login form

--- src/components/superAdmin/auth/Login.jsx
+++ src/components/superAdmin/auth/Login.jsx
@@ -3,57 +3,55 @@ import Button from "../../../commons/form/Button";
 import Input from "../../../commons/form/Input";
 
 const Login = () => {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
 
-  const onHandleChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const onHandleChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
+    const onHandleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+    const onHandleChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
 
-  return (
-    <>
-      <div className="form-group">
-        <Input
-          labelFor={"Email"}
-          type={"email"}
-          label={"email"}
-          placeholder={"me@example.com"}
-          required={true}
-          name={"email"}
-          id={"email"}
-          value={email}
-          style={"mb-3 mt-1"}
-          handleChange={onHandleChangeEmail}
-        />
+    return (
+        <>
+            <Input
+                labelFor={"Email"}
+                type={"email"}
+                label={"email"}
+                placeholder={"me@example.com"}
+                required={true}
+                name={"email"}
+                id={"email"}
+                value={email}
+                style={"mb-3 mt-1 col-lg-8"}
+                handleChange={onHandleChangeEmail}
+            />
 
-        <Input
-          labelFor={"Password"}
-          type={"password"}
-          label={"password"}
-          placeholder={"********"}
-          required={true}
-          name={"password"}
-          id={"password"}
-          value={password}
-          style={"mb-3 mt-1"}
-          handleChange={onHandleChangePassword}
-        />
+            <Input
+                labelFor={"Password"}
+                type={"password"}
+                label={"password"}
+                placeholder={"********"}
+                required={true}
+                name={"password"}
+                id={"password"}
+                value={password}
+                style={"mb-3 mt-1"}
+                handleChange={onHandleChangePassword}
+            />
 
-        <div className="text-center mt-2">
-          <Button
-            type={"submit"}
-            text={"Login"}
-            loading={false}
-            style={"btn btn-primary px-5"}
-            disabled={false}
-          />
-        </div>
-      </div>
-    </>
-  );
+            <div className="text-center mt-2">
+                <Button
+                    type={"submit"}
+                    text={"Login"}
+                    loading={false}
+                    style={"btn btn-primary px-5"}
+                    disabled={false}
+                />
+            </div>
+        </>
+    );
 };
 
 export default Login;
--- src/views/superAdmin/Login.jsx
+++ src/views/superAdmin/Login.jsx
@@ -1,16 +1,17 @@
 import Login from "../../components/superAdmin/auth/Login";
+import "../../index.css";
 
 const SuperAdminLogin = () => {
-  return (
-    <div className="container mt-4">
-      <div className="row justify-content-center align-items-center">
-        <div className="col-md-6 border px-5 pt-5 pb-2">
-          <h3 className="text-primary text-center">Super Admin Login</h3>
-          <Login />
+    return (
+        <div className="container mt-4">
+            <div className="card p-5">
+                <h3 className="text-primary text-center">Super Admin Login</h3>
+                <div className="card-body">
+                    <Login />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SuperAdminLogin;
