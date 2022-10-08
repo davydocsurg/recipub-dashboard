0cc16283e0927939e3f34079b247b743a22a4352
davydocsurg <davydocsurg@gmail.com>
2022-10-08 17:18:23 +0100

    add reusable input changes

--- src/commons/form/Input.jsx
+++ src/commons/form/Input.jsx
@@ -1,43 +1,50 @@
 import React from "react";
-import Label from "./Label";
+import Label from "./label";
 import FieldError from "./FieldError";
 
 const Input = ({
-  labelFor,
-  type,
-  style,
-  label,
-  placeholder,
-  value,
-  disabled,
-  required,
-  name,
-  id,
-  error,
+    labelFor,
+    type,
+    style,
+    value,
+    label,
+    placeholder,
+    disabled,
+    required,
+    name,
+    id,
+    error,
+    handleChange,
 }) => {
-  return (
-    <>
-      <div className="form-group">
-        {labelFor && (
-          <Label htmlFor={name} inputName={labelFor} required={required} />
-        )}
+    return (
+        <>
+            <div className="form-group">
+                {labelFor && (
+                    <Label
+                        htmlFor={name}
+                        inputName={labelFor}
+                        required={required}
+                    />
+                )}
 
-        <div className="mt-1">
-          <input
-            type={type}
-            aria-label={label}
-            placeholder={placeholder}
-            className={"form-control " + style}
-            disabled={disabled}
-            required={required}
-            name={name}
-            id={id}
-          />
-        </div>
-        {error && <FieldError>{error}</FieldError>}
-      </div>
-    </>
-  );
+                <div className="mt-1">
+                    <input
+                        type={type}
+                        aria-label={label}
+                        placeholder={placeholder}
+                        className={"form-control " + style}
+                        disabled={disabled}
+                        required={required}
+                        name={name}
+                        id={id}
+                        value={value}
+                        onChange={handleChange}
+                    />
+                </div>
+                {error && <FieldError>{error}</FieldError>}
+            </div>
+        </>
+    );
 };
 
 export default Input;
